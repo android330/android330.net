d880172eacf070890cd4c171e1c88f1b7243017a
Andrew Beauchemin <android@vt.edu>
2024-08-04 21:45:58 -0400

    Added router

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import { Box, Container } from '@mui/material';
+
+
+export default function Home(){
+
+    return(
+        <Box  
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="100vh"
+            borderColor="black"
+        >
+                Test
+      </Box>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, createBrowserRouter } from 'react-router-dom';
+import AppRoutes from './router/AppRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
