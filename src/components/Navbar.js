8912d912daadfd5f1eb59ffa93e1301013985315
Andrew Beauchemin <android@vt.edu>
2024-08-31 23:13:26 -0400

    commented out About Me Page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,12 @@ import { Navigate, useNavigate } from "react-router-dom"
 export default function Navbar(){
     const navigate = useNavigate();
 
+
+    // <Link variant="h6"  sx={{paddingLeft: 2, color: "white", cursor: "pointer"}} onClick={()=>{navigate('/AboutMe')}}>About Me</Link>
     return(
         <Box width="100%" sx={{display: "flex",flexDirection: "row", background: "black"}}>
           <Link variant="h6" sx={{paddingLeft: 2, color: "white", cursor: "pointer"}} onClick={()=>{navigate('/')}}>Projects</Link>
-          <Link variant="h6"  sx={{paddingLeft: 2, color: "white", cursor: "pointer"}} onClick={()=>{navigate('/AboutMe')}}>About Me</Link>
+          
         </Box>
     )
 }
