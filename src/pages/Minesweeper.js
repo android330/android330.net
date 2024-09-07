8f8234b09813deb7a6e80b07fb2af678ed76abbf
Andrew Beauchemin <android@vt.edu>
2024-09-07 18:49:33 -0400

    added board creation and test board images

--- src/pages/Minesweeper.js
+++ src/pages/Minesweeper.js
@@ -1,14 +1,48 @@
-import { useEffect } from "react"
+import { Box, Container, Stack } from "@mui/material";
+import { useEffect, useState } from "react"
 
+
+const rows = 12;
+const columns = 10;
+const mines = 10;
+
+function getAdjacentTiles(index){
+    let adjacentArray = []
+
+    if(index-columns >= 0){
+        adjacentArray.push(index-columns);
+        if((index-columns+1) % columns != 0){
+            adjacentArray.push(index-columns+1);
+        }
+        if((index-columns) % columns != 0){
+            adjacentArray.push(index-columns-1);
+        }
+    }
+    if(index+columns < rows * columns){
+        adjacentArray.push(index+columns);
+        if((index+columns+1) % columns != 0){
+            adjacentArray.push(index+columns+1);
+        }
+        if((index+columns) % columns != 0){
+            adjacentArray.push(index+columns-1);
+        }
+    }
+    if((index+1) % columns != 0){
+        adjacentArray.push(index+1);
+    }
+    if(index % columns != 0){
+        adjacentArray.push(index-1);
+    }
+
+    return adjacentArray;
+}
 
 function generateBoard(rows, columns, mines){
     let boardSize = rows * columns;
-    let board = [];
+    let board = [boardSize - 1];
     for(let i = 0; i < boardSize; i++){
         board[i] = 0;
     }
-
-    
 
     let minesToPlant = mines;
     while(minesToPlant > 0){
@@ -19,20 +53,44 @@ function generateBoard(rows, columns, mines){
         }
     }
 
+    console.log(board);
+    for(let i = 0; i < boardSize; i++){
+        if(board[i] == 'x'){
+            let adjacentTiles = getAdjacentTiles(i, board);
+            console.log(adjacentTiles)
+            for(let x = 0; x < adjacentTiles.length; x++){
+                if(board[adjacentTiles[x]] != 'x'){
+                    board[adjacentTiles[x]] = board[adjacentTiles[x]] + 1;
+                }
+            }
+        }
+    }
+
+    
     return board;
 }
 
 export default function Minesweeeper(){
+    
+    var [board, setBoard] = useState([]);
+
     useEffect(()=>{
-        const rows = 10;
-        const columns = 10;
-        const mines = 20;
-        let board = generateBoard(rows, columns, mines);
         
-        console.log(board)
+        setBoard(generateBoard(rows, columns, mines));
     },[])
 
     return(
-        <p>test</p>
+        <Container component="main" maxWidth="lg">
+            {board.map((tile, index)=>(
+                <>
+                    <Box key={index} 
+                        sx={{marginBottom: -1, marginRight: "1px", cursor: "pointer", userSelect: "none", MozUserSelect: "none"}} 
+                        component="img" 
+                        src={tile == 'x' ? "./files/mine.png" : ("./files/" + tile + ".png")} 
+                        onClick={()=>console.log(index)}/>
+                    {(index + 1) % columns == 0 && <br/>}
+                </>
+            ))}
+        </Container>
     )
 }
